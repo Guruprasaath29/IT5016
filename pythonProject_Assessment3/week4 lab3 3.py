a9644844bc7db95e5d167cd47693f0bdebc296e7
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:33:00 +1200

    Update week4 lab3 3.py

--- pythonProject_Assessment3/week4 lab3 3.py
+++ pythonProject_Assessment3/week4 lab3 3.py
@@ -1,12 +1,21 @@
+# Define a function named calculate_average that takes a variable number of arguments
 def calculate_average(*args):
+    # Calculate the total marks by summing all the arguments
     total_marks = sum(args)
+    # Count the number of subjects (arguments)
     num_subjects = len(args)
+    # Calculate the average by dividing the total marks by the number of subjects
     average = total_marks / num_subjects
+    # Print the average
     print("The average is:", average)
+    
+    # Check if the average is greater than or equal to 50
     if average >= 50:
-         print("pass")
+        # If the average is greater than or equal to 50, print "pass"
+        print("Pass")
     else:
-        print("fail")
+        # If the average is less than 50, print "fail"
+        print("Fail")
 
-calculate_average(20,40,30)
-
+# Call the calculate_average function with marks as arguments
+calculate_average(20, 40, 30)
