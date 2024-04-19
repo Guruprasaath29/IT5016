1039bb2253fb40453ff87facb57cc0791fac1515
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:17:44 +1200

    Update activity.py

--- pythonProject_Assessment3/activity.py
+++ pythonProject_Assessment3/activity.py
@@ -1,7 +1,15 @@
-m=str(input("Enter your message:"))
-count=0
+# Prompt the user to enter a message
+m = str(input("Enter your message:"))
+
+# Initialize a variable to store the count of vowels
+count = 0
+
+# Loop through each character in the message
 for l in m:
+    # Check if the character is a vowel (a, e, i, o, or u)
     if l == "a" or l == "e" or l == "i" or l == "o" or l == "u":
+        # If the character is a vowel, increment the count
         count += 1
 
-print(" vowels:", count)
+# Print the count of vowels in the message
+print("vowels:", count)
