d1d9a913840ba092a6dc62707275609478dda5b5
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:39:53 +1200

    Update week55.py

--- pythonProject_Assessment3/week55.py
+++ pythonProject_Assessment3/week55.py
@@ -1,7 +1,14 @@
+# Define a function named percentage that takes a variable number of arguments
 def percentage(*args):
-   total = sum(args)
-   avg = total / len(args)
-   return avg
-# Example usage:
+    # Calculate the total sum of the arguments
+    total = sum(args)
+    # Calculate the average by dividing the total sum by the number of arguments
+    avg = total / len(args)
+    # Return the average
+    return avg
+
+# Example usage of the percentage function
+# Calculate the average of the numbers 56, 61, and 73
 avg = percentage(56, 61, 73)
-print('average =', avg)
+# Print the calculated average
+print('Average =', avg)
