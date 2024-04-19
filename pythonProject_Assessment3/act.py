227e0dd4d660204e6179a29a66e1c31ca8ac3897
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:16:10 +1200

    Update act.py

--- pythonProject_Assessment3/act.py
+++ pythonProject_Assessment3/act.py
@@ -1,18 +1,25 @@
+# Initialize an empty list to store city names
 city = []
+
+# Prompt the user to enter city names five times and add them to the list
 for i in range(5):
     city_name = input("Enter a city name: ")
     city.append(city_name)
 
-
+# Print the list of cities
 print("Cities:", city)
 
+# Print the length of the list
 print("Length of the array:", len(city))
 
+# Add a new city ("Hong") to the end of the list
 city.append("Hong")
 print("Updated Cities:", city)
 
+# Remove the first city from the list
 city.pop(0)
 print("Cities after removing the first element:", city)
 
+# Reverse the order of the cities in the list
 city.reverse()
 print("Reversed array:", city)
