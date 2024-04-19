f17c7f8c0cd9c9cfa257c1d882ed59caadeede2e
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:41:18 +1200

    Update week6 2.py

--- pythonProject_Assessment3/week6 2.py
+++ pythonProject_Assessment3/week6 2.py
@@ -1,19 +1,25 @@
+# Define a class named Cars
 class Cars:
-    # initialise the function
+    # Define the constructor method to initialize name, speed, and mileage attributes
     def __init__(self, name, speed, mileage):
         self.name = name
         self.speed = speed
         self.mileage = mileage
 
+    # Define a method named car to return information about the car's name
     def car(self):
-        return (" The name of the car is " + self.name)
+        return "The name of the car is " + self.name
 
+    # Define a method named mil to convert mileage from miles to kilometers
     def mil(self):
-        Kilometer  = self.mileage * 10
-        return (Kilometer)
+        # Convert mileage from miles to kilometers (assuming 1 mile = 10 kilometers)
+        kilometers = self.mileage * 10
+        return kilometers
 
-
+# Create an instance of the Cars class with name "Toyota Mark X", speed "150km/hr", and mileage 15.50 miles
 i = Cars("Toyota Mark X", "150km/hr", 15.50)
 
+# Call the car method of the Cars instance to get information about the car's name
 print(i.car())
+# Call the mil method of the Cars instance to convert the mileage to kilometers
 print(i.mil())
