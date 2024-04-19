7560e2c9c3100f4735f983623c8e09a369542f97
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:39:04 +1200

    Update week5 3.py

--- pythonProject_Assessment3/week5 3.py
+++ pythonProject_Assessment3/week5 3.py
@@ -1,10 +1,17 @@
+# Define a function named average_student that takes keyword arguments (**kwargs)
 def average_student(**kwargs):
-    sum=0
-
+    # Initialize a variable sum to store the sum of scores
+    sum = 0
+    # Iterate over the values of the keyword arguments (scores)
     for i in kwargs.values():
-        sum= sum+i
-        avg= sum/len(kwargs)
+        # Calculate the sum of scores
+        sum += i
+    # Calculate the average by dividing the sum by the number of scores
+    avg = sum / len(kwargs)
+    # Return both the sum and average
     return sum, avg
 
+# Call the average_student function with keyword arguments representing scores for different subjects
 k = average_student(IT5014=60, IT7809=80, IT6798=50, IT5048=70)
-print("Sum & average=", k)
+# Print the sum and average returned by the function
+print("Sum & average:", k)
