5a053e31f7d9fc1a1451aaafaeaf0ab3ef0dbe0c
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:27:39 +1200

    Update lab Week4.py

--- pythonProject_Assessment3/lab Week4.py
+++ pythonProject_Assessment3/lab Week4.py
@@ -1,9 +1,21 @@
-student_id = input("Enter the student id:")
-student_fn = input("Enter the first name of the student:")
-student_ln = input("Enter the last name of the student:")
+# Prompt the user to enter the student ID
+student_id = input("Enter the student ID: ")
+# Prompt the user to enter the first name of the student
+student_fn = input("Enter the first name of the student: ")
+# Prompt the user to enter the last name of the student
+student_ln = input("Enter the last name of the student: ")
+
+# Define a message
 message = "I am a newbie In Whitecliffe college !"
+# Split the message into words
 words = message.split()
-print("words=", words)
+
+# Print the words in the message
+print("Words in the message:", words)
+
+# Iterate through each word in the message
 for i in words:
-        if i == "Whitecliffe" or i == "college":
-            print(i)
+    # Check if the word is "Whitecliffe" or "college"
+    if i == "Whitecliffe" or i == "college":
+        # If the word is "Whitecliffe" or "college", print it
+        print(i)
