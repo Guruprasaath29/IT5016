827a73887dc870e2f96e797a4ad41310b3c89cbf
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:19:35 +1200

    Update activity1.py

--- pythonProject_Assessment3/activity1.py
+++ pythonProject_Assessment3/activity1.py
@@ -1,8 +1,14 @@
+# input the user to enter a score within the range of 0 to 100
 score = int(input("Enter the score (0-100): "))
+
+# Validate the score to ensure it falls within the specified range
 while score < 0 or score > 100:
     score = int(input("Invalid score. Enter the score (0-100): "))
 
+# Initialize an empty string to store the grade
 grade = ""
+
+# Assign a grade based on the score
 while True:
     if score >= 80:
         grade = "A"
@@ -17,4 +23,5 @@ while True:
         grade = "Fail"
         break
 
-print("Grade:",grade)
+# Print the grade
+print("Grade:", grade)
