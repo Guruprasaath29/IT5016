e67ae207fc33d2a21422427de505d9b36176c697
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:24:44 +1200

    Update exercise.py

--- pythonProject_Assessment3/exercise.py
+++ pythonProject_Assessment3/exercise.py
@@ -1,11 +1,19 @@
+# Initialize empty lists to store student names and ages
 student_name = []
 student_age = []
 
+# Prompt the user to input student names and ages for 7 students
 for i in range(7):
+    # Input student name
     name = input("Enter student name: ")
-    age =int(input("Enter student age:"))
-student_name.append(name)
-student_age.append(age)
+    # Input student age
+    age = int(input("Enter student age: "))
+    
+    # Append the name and age to their respective lists
+    student_name.append(name)
+    student_age.append(age)
 
-print("student Names:",student_name)
-print("student Ages:",student_age)
+# Print the list of student names
+print("Student Names:", student_name)
+# Print the list of student ages
+print("Student Ages:", student_age)
