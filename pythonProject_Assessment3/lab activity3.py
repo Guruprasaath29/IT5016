df83b8d9c11b06f70f66a3f9b3f6d2794c2fd321
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:28:46 +1200

    Update lab activity3.py

--- pythonProject_Assessment3/lab activity3.py
+++ pythonProject_Assessment3/lab activity3.py
@@ -1,6 +1,10 @@
+# Define a list of cars
 cars = ["honda", "toyota", "peugeot"]
+# Define a list of colors
 colors = ["blue", "red", "pink", "black"]
 
+#  loop to iterate over each car and each color
 for car in cars:
     for color in colors:
+        # Print the combination of car and color
         print(car, color)
