b31fbe26344c8ec12404dfaadddc04e2e7081ea8
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:40:39 +1200

    Update week6 1.py

--- pythonProject_Assessment3/week6 1.py
+++ pythonProject_Assessment3/week6 1.py
@@ -1,10 +1,17 @@
+# Define a class named Fruit
 class Fruit:
+    # Define the constructor method to initialize name and color attributes
     def __init__(self, name, color):
         self.name = name
         self.color = color
 
+    # Define a method named func to print a message indicating the name of the fruit
     def func(self):
         print("Fruit is " + self.name)
 
-f1= Fruit("Banana", "Yellow")
+# Create an instance of the Fruit class with name "Banana" and color "Yellow"
+f1 = Fruit("Banana", "Yellow")
+
+# Call the func method of the Fruit instance
+# This will print "Fruit is Banana"
 print(f1.func())
