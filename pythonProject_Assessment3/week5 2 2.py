d92305e1af13b2206ca171adca5c1dd2efaf938b
Guruprasaath29 <160688651+Guruprasaath29@users.noreply.github.com>
2024-04-19 14:36:03 +1200

    Update week5 2 2.py

--- pythonProject_Assessment3/week5 2 2.py
+++ pythonProject_Assessment3/week5 2 2.py
@@ -1,5 +1,9 @@
+# Define a function named fruit_name that takes keyword arguments (**kwargs)
 def fruit_name(**kwargs):
+    # Iterate over the keys of the keyword arguments (fruit names)
     for i in kwargs.keys():
+        # Print each fruit name
         print(i)
 
+# Call the fruit_name function with keyword arguments representing fruit names and quantities
 fruit_name(mangoes=1, oranges=2, apples=3)
